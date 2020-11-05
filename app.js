3bc6f87cd634d5b54a1a1b85e4e18176dffdab56
nageshbk <nageshbk1@gmail.com>
2020-11-05 20:07:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("xxxxxxxxxxxxxxxpppppppppppppwuuuuuuuuuuuelgggggh");
+});
+app.listen(3000, () => {
+    console.log("dfffffff  njjjjjjjj");
+});
