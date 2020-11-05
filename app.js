95d09c9c6ca4b44d4f33b852d8a67f43b6b59140
nageshbk <nageshbk1@gmail.com>
2020-11-05 20:33:55 +0530

    port preference

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
+const port = process.env.port || 3000;
 const express = require('express');
 const app = express();
+const packageName = require('packageName')
 
 app.get('/', (req, res) => {
-    res.send("xxxxxxxxxxxxxxxpppppppppppppwuuuuuuuuuuuelgggggh");
+    res.send("hey my web site");
 });
-app.listen(3000, () => {
-    console.log("dfffffff  njjjjjjjj");
+app.listen(port, () => {
+    console.log("listening");
 });
