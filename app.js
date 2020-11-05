bd9cad4fddda18c86c681f5e40c7bdc8f84f7a3b
nageshbk <nageshbk1@gmail.com>
2020-11-05 23:08:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const port = process.env.port || 3000;
 const express = require('express');
 const app = express();
-const packageName = require('packageName')
+
 
 app.get('/', (req, res) => {
     res.send("hey my web site");
